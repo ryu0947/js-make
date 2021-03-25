ea0f586db4849e707301a72af282fd0f1b1393df
ryu <dragonogawaz@gmail.com>
2021-03-25 22:55:45 +0900

    ハンバーガーメニューとドロワー用のID追加

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,7 +2,8 @@ const hamburgerBtn = document.getElementById("js-hamburger");
 const topLine = document.getElementById("js-top-line");
 const centerLine = document.getElementById("js-center-line");
 const bottomLine = document.getElementById("js-bottom-line");
-const nav = document.getElementById("js-nav");
+const hamburgerNav = document.getElementById("js-hamburger-nav");
+const drawerNav = document.getElementById("js-drawer-nav");
 
 hamburgerBtn.addEventListener("click", () => {
   topLine.classList.toggle("active");
