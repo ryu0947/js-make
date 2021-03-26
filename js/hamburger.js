55fe7918d697541f9b6f1c22d7b8f2d8469294cc
ryu <dragonogawaz@gmail.com>
2021-03-27 02:16:28 +0900

    ハンバーガーメニューの実装

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,32 +1,11 @@
-const hamburgerBtn = document.getElementById("js-hamburger");
-const drawerBtn = document.getElementById("js-drawer");
-const hamburgerNav = document.getElementById("js-hamburger-nav");
-const drawerNav = document.getElementById("js-drawer-nav");
-const hamburgerTop = document.getElementById("hamburger-top");
-const hamburgerCenter = document.getElementById("hamburger-center");
-const hamburgerBottom = document.getElementById("hamburger-bottom");
-const drawerTop = document.getElementById("drawer-top");
-const drawerCenter = document.getElementById("drawer-center");
-const drawerBottom = document.getElementById("drawer-bottom");
+const nav = document.getElementById("js-nav");
+const navBtn = document.getElementById("js-nav-btn");
+const navrBtnItems = document.querySelectorAll(".nav-btn-item");
 
-function hamburgerActive() {
-  hamburgerTop.classList.toggle("active");
-  hamburgerCenter.classList.toggle("active");
-  hamburgerBottom.classList.toggle("active");
-  hamburgerNav.classList.toggle("show");
-}
-
-function drawerActive() {
-  drawerTop.classList.toggle("active");
-  drawerCenter.classList.toggle("active");
-  drawerBottom.classList.toggle("active");
-  drawerNav.classList.toggle("show");
-}
-
-hamburgerBtn.addEventListener("click", () => {
-  hamburgerActive();
-});
-
-drawerBtn.addEventListener("click", () => {
-  drawerActive();
+navBtn.addEventListener("click", () => {
+  navrBtnItems.forEach((navrBtnItem) => {
+    navrBtnItem.classList.toggle("active");
+  });
+  nav.classList.toggle("show");
+  navBtn.classList.toggle("active");
 });
