b9f4c27eaf652a556e71f82fd85306f04605d577
ryu <dragonogawaz@gmail.com>
2021-03-29 07:35:48 +0900

    一定量スクロールしたらトップへ戻るボタンを表示する

--- js/top-btn.js
+++ js/top-btn.js
@@ -2,6 +2,16 @@
 
 const topBtn = document.getElementById("js-top-btn");
 
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+
+  if (scrollY > 200) {
+    topBtn.classList.add("show");
+  } else {
+    topBtn.classList.remove("show");
+  }
+});
+
 topBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
