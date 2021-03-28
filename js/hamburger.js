c16acbb8e853dc73c0442c9e1fb6e1522b40c670
ryu <dragonogawaz@gmail.com>
2021-03-28 20:55:04 +0900

    クリックイベントの追加

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,7 @@
 const nav = document.getElementById("js-nav");
 const navBtn = document.getElementById("js-nav-btn");
 const navrBtnItems = document.querySelectorAll(".nav-btn-item");
+const bodyElem = document.querySelector("body");
 
 navBtn.addEventListener("click", () => {
   navrBtnItems.forEach((navrBtnItem) => {
@@ -8,4 +9,5 @@ navBtn.addEventListener("click", () => {
   });
   nav.classList.toggle("show");
   navBtn.classList.toggle("active");
+  bodyElem.classList.toggle("mask");
 });
