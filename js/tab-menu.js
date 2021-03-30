635818720bfc0087c596cdc7041a6dc72ca12c38
ryu <dragonogawaz@gmail.com>
2021-03-31 06:54:49 +0900

    タブメニューのJS追加

---
+++ js/tab-menu.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const tabItems = document.querySelectorAll(".tab-item");
+const tabInners = document.querySelectorAll(".tab-inner");
+
+tabItems.forEach((clickItem) => {
+  clickItem.addEventListener("click", () => {
+    tabItems.forEach((tabItem) => {
+      tabItem.classList.remove("active");
+    });
+
+    clickItem.classList.add("active");
+
+    tabInners.forEach((tabInner) => {
+      tabInner.classList.remove("active");
+    });
+
+    document.getElementById(clickItem.dataset.id).classList.add("active");
+  });
+});
