d5e55306b7d389821bba72f48c476b06cd6428fd
ryu <dragonogawaz@gmail.com>
2021-04-02 12:50:22 +0900

    メニューの開閉で＋と−を切り替える処理追加

--- js/accordion-menu.js
+++ js/accordion-menu.js
@@ -1,9 +1,16 @@
 "use strict";
 
 const accordionBtn = document.querySelectorAll(".accordion-btn");
+const accordionLine = document.querySelectorAll(".accordion-line");
 
 accordionBtn.forEach((clickBtn) => {
   clickBtn.addEventListener("click", () => {
-    document.getElementById(clickBtn.dataset.id).classList.toggle("open");
+    document.getElementById(clickBtn.dataset.btn).classList.toggle("open");
+
+    accordionLine.forEach((clickLine) => {
+      if (clickLine.classList.contains(clickBtn.dataset.line)) {
+        clickLine.classList.toggle("active");
+      }
+    });
   });
 });
