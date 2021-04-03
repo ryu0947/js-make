25d2f53499e4918a07ace8f3e6b584165475e271
ryu <dragonogawaz@gmail.com>
2021-04-03 23:23:52 +0900

    ハンバーガーメニューを開いた時にスクロール出来ないようにする

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,13 +1,26 @@
 const nav = document.getElementById("js-nav");
 const navBtn = document.getElementById("js-nav-btn");
 const navrBtnItems = document.querySelectorAll(".nav-btn-item");
-const bodyElem = document.querySelector("body");
+const body = document.body;
+
+function scrollFixed() {
+  if (nav.classList.contains("show")) {
+    const scrollY = window.pageYOffset;
+    body.style.position = "fixed";
+    body.style.top = `-${scrollY}px`;
+  } else {
+    const scrollY = body.style.top;
+    body.style.position = "";
+    body.style.top = "";
+    window.scrollTo(0, parseInt(scrollY) * -1);
+  }
+}
 
 navBtn.addEventListener("click", () => {
   navrBtnItems.forEach((navrBtnItem) => {
     navrBtnItem.classList.toggle("active");
   });
   nav.classList.toggle("show");
-  navBtn.classList.toggle("active");
-  bodyElem.classList.toggle("mask");
+  body.classList.toggle("mask");
+  scrollFixed();
 });
