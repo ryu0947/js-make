50cd43a8e332ab61d354acb433fd855ce833d45e
ryu <dragonogawaz@gmail.com>
2021-04-04 08:59:20 +0900

    コード全体をブロックで囲ってグローバルスコープの干渉を防ぐ

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,27 +1,31 @@
-const nav = document.getElementById("js-nav");
-const navBtn = document.getElementById("js-nav-btn");
-const navBtnItems = document.querySelectorAll(".nav-btn-item");
-const body = document.body;
+"use strict";
 
-function scrollFixed() {
-  if (nav.classList.contains("show")) {
-    const scrollY = window.pageYOffset;
-    body.style.position = "fixed";
-    body.style.top = `-${scrollY}px`;
-  } else {
-    const scrollY = body.style.top;
-    body.style.position = "";
-    body.style.top = "";
-    window.scrollTo(0, parseInt(scrollY) * -1);
+{
+  const nav = document.getElementById("js-nav");
+  const navBtn = document.getElementById("js-nav-btn");
+  const navBtnItems = document.querySelectorAll(".nav-btn-item");
+  const body = document.body;
+
+  function scrollFixed() {
+    if (nav.classList.contains("show")) {
+      const scrollY = window.pageYOffset;
+      body.style.position = "fixed";
+      body.style.top = `-${scrollY}px`;
+    } else {
+      const scrollY = body.style.top;
+      body.style.position = "";
+      body.style.top = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
+    }
   }
-}
 
-navBtn.addEventListener("click", () => {
-  navBtnItems.forEach((navrBtnItem) => {
-    navrBtnItem.classList.toggle("active");
+  navBtn.addEventListener("click", () => {
+    navBtnItems.forEach((navrBtnItem) => {
+      navrBtnItem.classList.toggle("active");
+    });
+    nav.classList.toggle("show");
+    body.classList.toggle("mask");
+    navBtn.classList.toggle("active");
+    scrollFixed();
   });
-  nav.classList.toggle("show");
-  body.classList.toggle("mask");
-  navBtn.classList.toggle("active");
-  scrollFixed();
-});
+}
--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,22 @@
 "use strict";
+{
+  const modal = document.getElementById("js-modal");
+  const openBtn = document.getElementById("js-open");
+  const closeBtn = document.getElementById("js-close");
+  const body = document.body;
 
-const modal = document.getElementById("js-modal");
-const openBtn = document.getElementById("js-open");
-const closeBtn = document.getElementById("js-close");
-
-openBtn.addEventListener("click", () => {
+  openBtn.addEventListener("click", () => {
     modal.classList.add("show");
-});
+    const scrollY = window.pageYOffset;
+    body.style.position = "fixed";
+    body.style.top = `-${scrollY}px`;
+  });
 
-closeBtn.addEventListener("click", () => {
+  closeBtn.addEventListener("click", () => {
     modal.classList.remove("show");
-});
+    const scrollY = body.style.top;
+    body.style.position = "";
+    body.style.top = "";
+    window.scrollTo(0, parseInt(scrollY || "0") * -1);
+  });
+}
