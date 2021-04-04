f74e277c5dacc27640a0ae8fb1fbcd700a3199f7
ryu <dragonogawaz@gmail.com>
2021-04-04 23:18:57 +0900

    モーダルの背景を固定する

--- js/modal.js
+++ js/modal.js
@@ -4,19 +4,21 @@
   const openBtn = document.getElementById("js-open");
   const closeBtn = document.getElementById("js-close");
   const body = document.body;
+  let scrollPos = 0;
+  let checked = false;
 
   openBtn.addEventListener("click", () => {
     modal.classList.add("show");
     const scrollY = window.pageYOffset;
+    scrollPos = scrollY;
     body.style.position = "fixed";
     body.style.top = `-${scrollY}px`;
   });
 
   closeBtn.addEventListener("click", () => {
     modal.classList.remove("show");
-    const scrollY = body.style.top;
     body.style.position = "";
     body.style.top = "";
-    window.scrollTo(0, parseInt(scrollY || "0") * -1);
+    window.scrollTo(0, scrollPos * 1);
   });
 }
