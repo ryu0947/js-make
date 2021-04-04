2a5c58bde4e0710dcb25479fb60f1c7806f2ccb8
ryu <dragonogawaz@gmail.com>
2021-04-04 23:01:28 +0900

    ハンバーガーメニューの背景固定

--- js/hamburger.js
+++ js/hamburger.js
@@ -5,13 +5,30 @@
   const navBtn = document.getElementById("js-nav-btn");
   const navBtnItems = document.querySelectorAll(".nav-btn-item");
   const body = document.body;
+  let scrollPos = 0;
+  let checked = false;
 
   function scrollFixed() {
     if (nav.classList.contains("show")) {
+      if (body.style.top) {
+        const scrollY = body.style.top;
+        body.style.position = "fixed";
+        body.style.top = `-${scrollY}px`;
+        scrollPos = scrollY;
+        checked = true;
+        return;
+      }
       const scrollY = window.pageYOffset;
       body.style.position = "fixed";
       body.style.top = `-${scrollY}px`;
     } else {
+      if (checked) {
+        const scrollY = scrollPos;
+        body.style.position = "";
+        body.style.top = "";
+        window.scrollTo(0, parseInt(scrollY || "0") * -1);
+        return;
+      }
       const scrollY = body.style.top;
       body.style.position = "";
       body.style.top = "";
