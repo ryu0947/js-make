1d0e913b486fa5893495e63214beab786ea25b4b
ryu <dragonogawaz@gmail.com>
2021-04-06 18:17:40 +0900

    IEとSafariでスムーススクロールを反映させるように設定

--- js/page-links.js
+++ js/page-links.js
@@ -9,6 +9,7 @@ const modal = document.getElementById("js-modal");
 const openBtn = document.getElementById("js-open");
 const closeBtn = document.getElementById("js-close");
 const body = document.body;
+const header = document.getElementById("js-header");
 
 function closeDrawerMenu() {
   if (nav.classList.contains("show")) {
@@ -39,9 +40,8 @@ pageLink.forEach((a) => {
     const href = a.getAttribute("href");
     const getTargetId = document.getElementById(href.replace("#", ""));
     const getElementPosition = getTargetId.getBoundingClientRect().top;
-    const scrollPos = window.pageYOffset;
-    const headerHeight = 90;
-    const calcElementPosition = getElementPosition + scrollPos - headerHeight;
+    const headerHeight = header.offsetHeight;
+    const calcElementPosition = getElementPosition + window.pageYOffset - headerHeight;
     window.scrollTo({
       top: calcElementPosition,
       left: 0,
