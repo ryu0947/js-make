c43322b041c17f9a3c3d04bfd77d7dfc37952ebf
ryu <dragonogawaz@gmail.com>
2021-04-06 15:29:22 +0900

    ドロワーを開いた時のスムーススクロールの挙動の制御

--- js/smooth.js
+++ js/smooth.js
@@ -1,17 +1,47 @@
 "use strict";
 
-const pcNav = document.querySelectorAll(".pc-list li a");
-const spNav = document.querySelectorAll(".sp-list li a");
+const pageLink = document.querySelectorAll("a[href$='link']");
+const nav = document.getElementById("js-nav");
+const navBtn = document.getElementById("js-nav-btn");
+const navBtnItems = document.querySelectorAll(".nav-btn-item");
+const mask = document.getElementById("js-mask");
+const modal = document.getElementById("js-modal");
+const openBtn = document.getElementById("js-open");
+const closeBtn = document.getElementById("js-close");
+const body = document.body;
 
-pcNav.forEach((a) => {
+function closeDrawerMenu() {
+  if (nav.classList.contains("show")) {
+    body.style.position = "";
+    body.style.top = "";
+    navBtnItems.forEach((navrBtnItem) => {
+      navrBtnItem.classList.toggle("active");
+    });
+    nav.classList.remove("show");
+    mask.classList.remove("show");
+    navBtn.classList.remove("active");
+  }
+}
+
+function closeModalMenu() {
+  if (modal.classList.contains("show")) {
+    body.style.position = "";
+    body.style.top = "";
+    modal.classList.remove("show");
+  }
+}
+
+pageLink.forEach((a) => {
   a.addEventListener("click", (e) => {
     e.preventDefault();
+    closeDrawerMenu();
+    closeModalMenu();
     const href = a.getAttribute("href");
     const getTargetId = document.getElementById(href.replace("#", ""));
     const getElementPosition = getTargetId.getBoundingClientRect().top;
-    const scrollY = window.pageYOffset;
+    const scrollPos = window.pageYOffset;
     const headerHeight = 90;
-    const calcElementPosition = getElementPosition + scrollY - headerHeight;
+    const calcElementPosition = getElementPosition + scrollPos - headerHeight;
     window.scrollTo({
       top: calcElementPosition,
       left: 0,
